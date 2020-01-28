3ff2bab0433744abc94cab63b8b7d11255d4aed3
Brendan LE BERRE <leberre.brendan@gmail.com>
2020-01-28 16:32:57 +0100

    Coordinates methods added

--- Coordinates.py
+++ Coordinates.py
@@ -1,2 +1,11 @@
+# Coordinates class
 class Coordinates(object):
-    
+
+    # Constructor
+    def __init__(self, latitude, longitude):
+        self.latitude = latitude
+        self.longitude = longitude
+
+    # Returns a list of coordinates [x, y]
+    def getCoordinates(self):
+        return [self.latitude, self.longitude]
